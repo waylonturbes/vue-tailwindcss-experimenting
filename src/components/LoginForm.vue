c5d714033adf3aa5d104e21275649ba61b2cb22c
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-02-01 21:42:42 -0800

    feat: simple form and initialized tailwindCSS

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,35 @@
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const username = ref("");
+    const password = ref("");
+    return {
+      username,
+      password,
+    };
+  },
+};
+</script>
+
+<template>
+  <form>
+    <h1>{{ username + password }}</h1>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <div>
+      <button @click="submitForm" :disabled="username.length == 0">
+        Submit
+      </button>
+    </div>
+  </form>
+</template>
+
+<style scoped></style>
